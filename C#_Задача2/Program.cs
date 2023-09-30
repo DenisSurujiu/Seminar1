458c33534e0f754ad22f0704f5b54b22bdeb3a10
Denis Surujiu <surujiudenis254@gmail.com>
2023-09-30 11:32:31 +0300

    Добавил вторую задачу

---
+++ C#_Задача2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace C__Задача2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//Задача 13: Напишите программу, которая выводит
+//третью цифру заданного числа или сообщает, что третьей
+//цифры нет.
+int num = new Random().Next(10,1000);
+Console.WriteLine(num);
+if (num>99)
+{
+    Console.WriteLine(num=num%10);
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
+        }
+    }
+}
