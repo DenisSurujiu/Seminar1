27a54f68a7f738e7ed9591bc386edbbebe13fe8a
Denis Surujiu <surujiudenis254@gmail.com>
2023-09-30 11:55:31 +0300

    Добавил третью задачу

---
+++ C#_Задача3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace C__Задача3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+//Задача 15: Напишите программу, которая принимает на
+//вход цифру, обозначающую день недели, и проверяет,
+//является ли этот день выходным.
+
+int num = new Random().Next(1,8);
+Console.WriteLine(num);
+if(num=6 || num=7)
+{
+    Console.WriteLine("(этот день выходной) -> да");
+}
+else
+{
+    Console.WriteLine("(этот не выходной) -> нет");
+}
+        }
+    }
+}
